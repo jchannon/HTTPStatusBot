e31fe9976059c8b3eb3c220c9675fe1fe578c7d8
Jonathan Channon <jonathan.channon@gmail.com>
2016-09-24 20:05:47 +0100

    hosting layers added

---
+++ HTTPStatusBot.SelfHosting/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HTTPStatusBot.SelfHosting
+{
+    using System;
+    using Nancy.Hosting.Self;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(new Uri("http://localhost:1234")))
+            {
+                host.Start();
+                Console.WriteLine("Running on http://localhost:1234");
+                Console.ReadLine();
+            }
+        }
+    }
+}
