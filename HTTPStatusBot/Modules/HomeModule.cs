2b66ecd2b9193f1fda12d42c312d89ab3eb9cd42
Jonathan Channon <jonathan.channon@gmail.com>
2016-09-26 19:24:28 +0100

    moved asyncness2

--- HTTPStatusBot/Modules/HomeModule.cs
+++ HTTPStatusBot/Modules/HomeModule.cs
@@ -1,5 +1,9 @@
 namespace HTTPStatusBot.Modules
 {
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Diagnostics;
+    using System.Security.Claims;
     using Extensions;
     using Microsoft.Bot.Builder.Dialogs;
     using Microsoft.Bot.Connector;
@@ -9,16 +13,84 @@ namespace HTTPStatusBot.Modules
 
     public class HomeModule : NancyModule
     {
+        public static string MicrosoftAppId { get; set; }
+        public static string MicrosoftAppIdSettingName { get; set; }
+        public static bool DisableSelfIssuedTokens { get; set; }
+        public static string OpenIdConfigurationUrl { get; set; } = JwtConfig.ToBotFromChannelOpenIdMetadataUrl;
 
         public HomeModule()
         {
-            this.RequiresBotAuthentication();
+           // this.RequiresBotAuthentication();
 
             Get("/", args => "Hi");
 
             Post("/", async _ =>
             {
+                MicrosoftAppId = MicrosoftAppId ??
+                               ConfigurationManager.AppSettings[MicrosoftAppIdSettingName ?? "MicrosoftAppId"];
+
+                if (Debugger.IsAttached && string.IsNullOrEmpty(MicrosoftAppId))
+                {
+                    // then auth is disabled
+                    return null;
+                }
+
+                var tokenExtractor =
+                    new JwtTokenExtractor(JwtConfig.GetToBotFromChannelTokenValidationParameters(MicrosoftAppId),
+                        OpenIdConfigurationUrl);
+
+                var identity = await tokenExtractor.GetIdentityAsync(this.Context.Request.Headers.Authorization);
+
+                // No identity? If we're allowed to, fall back to MSA
+                // This code path is used by the emulator
+                if (identity == null && !DisableSelfIssuedTokens)
+                {
+                    tokenExtractor = new JwtTokenExtractor(JwtConfig.ToBotFromMSATokenValidationParameters,
+                        JwtConfig.ToBotFromMSAOpenIdMetadataUrl);
+
+                    identity = await tokenExtractor.GetIdentityAsync(this.Context.Request.Headers.Authorization);
+
+                    // Check to make sure the app ID in the token is ours
+                    if (identity != null)
+                    {
+                        // If it doesn't match, throw away the identity
+                        if (tokenExtractor.GetBotIdFromClaimsIdentity(identity) != MicrosoftAppId)
+                        {
+                            identity = null;
+                        }
+                    }
+                }
+
+                // Still no identity? Fail out.
+                if (identity == null)
+                {
+                    //https://github.com/Microsoft/BotBuilder/blob/master/CSharp/Library/Microsoft.Bot.Connector/JwtTokenExtractor.cs#L87
+                    //tokenExtractor.GenerateUnauthorizedResponse(actionContext);
+                    return new Response
+                    {
+                        StatusCode = HttpStatusCode.Unauthorized,
+                        Headers =
+                            new Dictionary<string, string>()
+                            {
+                                {"WWW-Authenticate", $"Bearer realm=\"{this.Context.Request.Url.HostName}\""}
+                            }
+                    };
+                }
+
                 var activity = this.Bind<Activity>();
+
+                if (!string.IsNullOrWhiteSpace(activity.ServiceUrl))
+                {
+                    MicrosoftAppCredentials.TrustServiceUrl(activity.ServiceUrl);
+                }
+                else
+                {
+                    Trace.TraceWarning("No activity in the Bot Authentication Action Arguments");
+                }
+
+                this.Context.CurrentUser = new ClaimsPrincipal(identity);
+
+               // var activity = this.Bind<Activity>();
 
                 switch (activity.GetActivityType())
                 {
