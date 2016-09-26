f439c7a556fc2a879e970e34a87afa1797de22b1
Jonathan Channon <jonathan.channon@gmail.com>
2016-09-26 20:07:43 +0100

    moved authentication to module method

--- HTTPStatusBot/Modules/HomeModule.cs
+++ HTTPStatusBot/Modules/HomeModule.cs
@@ -4,6 +4,7 @@ namespace HTTPStatusBot.Modules
     using System.Configuration;
     using System.Diagnostics;
     using System.Security.Claims;
+    using System.Threading.Tasks;
     using Extensions;
     using Microsoft.Bot.Builder.Dialogs;
     using Microsoft.Bot.Connector;
@@ -20,77 +21,28 @@ namespace HTTPStatusBot.Modules
 
         public HomeModule()
         {
-           // this.RequiresBotAuthentication();
+            // this.RequiresBotAuthentication();
 
             Get("/", args => "Hi");
 
             Post("/", async _ =>
             {
-                MicrosoftAppId = MicrosoftAppId ??
-                               ConfigurationManager.AppSettings[MicrosoftAppIdSettingName ?? "MicrosoftAppId"];
 
-                if (Debugger.IsAttached && string.IsNullOrEmpty(MicrosoftAppId))
+                var valid = await this.BotAuthenticationValid();
+                if (!valid)
                 {
-                    // then auth is disabled
-                    return null;
-                }
-
-                var tokenExtractor =
-                    new JwtTokenExtractor(JwtConfig.GetToBotFromChannelTokenValidationParameters(MicrosoftAppId),
-                        OpenIdConfigurationUrl);
-
-                var identity = await tokenExtractor.GetIdentityAsync(this.Context.Request.Headers.Authorization);
-
-                // No identity? If we're allowed to, fall back to MSA
-                // This code path is used by the emulator
-                if (identity == null && !DisableSelfIssuedTokens)
-                {
-                    tokenExtractor = new JwtTokenExtractor(JwtConfig.ToBotFromMSATokenValidationParameters,
-                        JwtConfig.ToBotFromMSAOpenIdMetadataUrl);
-
-                    identity = await tokenExtractor.GetIdentityAsync(this.Context.Request.Headers.Authorization);
-
-                    // Check to make sure the app ID in the token is ours
-                    if (identity != null)
-                    {
-                        // If it doesn't match, throw away the identity
-                        if (tokenExtractor.GetBotIdFromClaimsIdentity(identity) != MicrosoftAppId)
-                        {
-                            identity = null;
-                        }
-                    }
-                }
-
-                // Still no identity? Fail out.
-                if (identity == null)
-                {
-                    //https://github.com/Microsoft/BotBuilder/blob/master/CSharp/Library/Microsoft.Bot.Connector/JwtTokenExtractor.cs#L87
-                    //tokenExtractor.GenerateUnauthorizedResponse(actionContext);
                     return new Response
                     {
                         StatusCode = HttpStatusCode.Unauthorized,
                         Headers =
-                            new Dictionary<string, string>()
-                            {
+                             new Dictionary<string, string>()
+                             {
                                 {"WWW-Authenticate", $"Bearer realm=\"{this.Context.Request.Url.HostName}\""}
-                            }
+                             }
                     };
                 }
 
                 var activity = this.Bind<Activity>();
-
-                if (!string.IsNullOrWhiteSpace(activity.ServiceUrl))
-                {
-                    MicrosoftAppCredentials.TrustServiceUrl(activity.ServiceUrl);
-                }
-                else
-                {
-                    Trace.TraceWarning("No activity in the Bot Authentication Action Arguments");
-                }
-
-                this.Context.CurrentUser = new ClaimsPrincipal(identity);
-
-               // var activity = this.Bind<Activity>();
 
                 switch (activity.GetActivityType())
                 {
@@ -101,6 +53,66 @@ namespace HTTPStatusBot.Modules
 
                 return HttpStatusCode.Accepted;
             });
+        }
+
+        private async Task<bool> BotAuthenticationValid()
+        {
+            MicrosoftAppId = MicrosoftAppId ??
+                ConfigurationManager.AppSettings[MicrosoftAppIdSettingName ?? "MicrosoftAppId"];
+
+            if (Debugger.IsAttached && string.IsNullOrEmpty(MicrosoftAppId))
+            {
+                // then auth is disabled
+                return true;
+            }
+
+            var tokenExtractor =
+                new JwtTokenExtractor(JwtConfig.GetToBotFromChannelTokenValidationParameters(MicrosoftAppId),
+                    OpenIdConfigurationUrl);
+
+            var identity = await tokenExtractor.GetIdentityAsync(this.Context.Request.Headers.Authorization);
+
+            // No identity? If we're allowed to, fall back to MSA
+            // This code path is used by the emulator
+            if (identity == null && !DisableSelfIssuedTokens)
+            {
+                tokenExtractor = new JwtTokenExtractor(JwtConfig.ToBotFromMSATokenValidationParameters,
+                    JwtConfig.ToBotFromMSAOpenIdMetadataUrl);
+
+                identity = await tokenExtractor.GetIdentityAsync(this.Context.Request.Headers.Authorization);
+
+                // Check to make sure the app ID in the token is ours
+                if (identity != null)
+                {
+                    // If it doesn't match, throw away the identity
+                    if (tokenExtractor.GetBotIdFromClaimsIdentity(identity) != MicrosoftAppId)
+                    {
+                        identity = null;
+                    }
+                }
+            }
+
+            // Still no identity? Fail out.
+            if (identity == null)
+            {
+                //https://github.com/Microsoft/BotBuilder/blob/master/CSharp/Library/Microsoft.Bot.Connector/JwtTokenExtractor.cs#L87
+                //tokenExtractor.GenerateUnauthorizedResponse(actionContext);
+                return true;
+            }
+
+            var activity = this.Bind<Activity>();
+
+            if (!string.IsNullOrWhiteSpace(activity.ServiceUrl))
+            {
+                MicrosoftAppCredentials.TrustServiceUrl(activity.ServiceUrl);
+            }
+            else
+            {
+                Trace.TraceWarning("No activity in the Bot Authentication Action Arguments");
+            }
+
+            this.Context.CurrentUser = new ClaimsPrincipal(identity);
+            return false;
         }
     }
 }
