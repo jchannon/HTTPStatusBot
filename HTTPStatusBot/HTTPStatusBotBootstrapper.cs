74e88e7cf2242753ce58c29085affac71f68c8e9
Jonathan Channon <jonathan.channon@gmail.com>
2016-09-26 16:53:59 +0100

    added on error to bootstrapper

---
+++ HTTPStatusBot/HTTPStatusBotBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿namespace HTTPStatusBot
+{
+    using System;
+    using Nancy;
+    using Nancy.Bootstrapper;
+    using Nancy.TinyIoc;
+
+    public class HTTPStatusBotBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            pipelines.OnError += (ctx, ex) =>
+            {
+                //Console.Error(ex.ToString());
+                return null;
+            };
+        }
+    }
+}
