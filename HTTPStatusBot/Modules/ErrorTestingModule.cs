9463947f8d8b4a2eeeaf406635994c581ca15580
Jonathan Channon <jonathan.channon@gmail.com>
2016-09-26 17:07:50 +0100

    added error module

---
+++ HTTPStatusBot/Modules/ErrorTestingModule.cs
@@ -0,0 +1,16 @@
+﻿namespace HTTPStatusBot.Modules
+{
+    using System;
+    using Nancy;
+    public class ErrorTestingModule : NancyModule
+    {
+        public ErrorTestingModule()
+        {
+            Get("/", _ =>
+            {
+                throw new Exception("oops");
+                return 200;
+            });
+        }
+    }
+}
